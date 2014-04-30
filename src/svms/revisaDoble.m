5bcbf074256393a4c90b043bcbe79c6fdf77c95e
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2014-04-29 20:02:56 -0400

    Corregida la función, se quita instrucción extra y se cambia la comparación por recomendación de Matlab.

--- src/svms/revisaDoble.m
+++ src/svms/revisaDoble.m
@@ -1,9 +1,7 @@
 function [DATA] = revisaDoble(DATATOT)
 
-tar = DATATOT(:,13:15);
-tar2 = tar(:,1) + tar(:,2) + tar(:,3);
-find(tar2 >=2);
-DATATOT(find(tar2 >=2),:) = [];
-
+tar = DATATOT(:,14:15);
+tar2 = tar(:,1) + tar(:,2);
+DATATOT(tar2 >= 2,:) = [];
 DATA = DATATOT;
 end
