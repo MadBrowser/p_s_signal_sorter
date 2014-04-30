17ef2db98da6e3506fd93ab8c27c9c1cb15666f4
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2014-04-29 20:05:32 -0400

    Corregido el nombre de las variables y se cambia identación de las líneas, además se corrigen errores ortográficos.

--- src/svms/iterSVM.m
+++ src/svms/iterSVM.m
@@ -1,26 +1,24 @@
 % funcion que genera la busqueda de grilla mas chica
-function [svmFinal Exac Cfinal Sfinal] = iterSVM(C,Sigma,DATAFIN)
+function [svmFinal, Exac, Cfinal, Sfinal] = iterSVM(C,S,DATAFIN)
 i=0;
 svmFinal = [];
 
-Exac( round(C(1,2)-C(1,1) + 1) , round( Sigma(1,2)-Sigma(1,1) + 1 ) ) = 0; 
-
+Exac(round(C(1,2) - C(1,1) + 1), round(S(1,2) - S(1,1) + 1 )) = 0; 
 
 for c = C(1,1):0.2:C(1,2)
     i = i + 1;
     j = 0;
-    for s = Sigma(1,1):0.2:Sigma(1,2)
+    for s = S(1,1):0.2:S(1,2)
         j = j + 1; 
-        [sist matriz] = SVM(DATAFIN,c,s);
-          
+        [sist, matriz] = SVM(DATAFIN,c,s);
         Exac(i,j) = calculaError(matriz,0);
-        %escoje a la mejor svm y se alamcenan sus datos
-        if(Exac(i,j) >= max(Exac(:)) )
+        % Escoje a la mejor svm y se almacenan sus datos
+        if(Exac(i,j) >= max(Exac(:)))
             Cfinal = c;
             Sfinal = s;
             svmFinal = sist;
         end
     end
-    
 end
+
 end
