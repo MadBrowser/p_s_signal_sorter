19ff49471132edee4e99fd1f7315d2b364452679
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2014-05-20 18:21:05 -0400

    Agregada información sobre la cantidad máxima de estaciones para entrenamiento, validación y prueba. Además se cambia el tamaño de la ventana a 8 segundos.

--- src/pre_processing/obtenerValores.m
+++ src/pre_processing/obtenerValores.m
@@ -3,11 +3,14 @@ function [total, intervalos] = obtenerValores(numeroMax)
 diary('Intervalos.txt');
 diary on
 
+% Total estaciones de entrenamiento + validación = 1590
+% Total estaciones de prueba = 1580
+
 total = 0;
 intervalos = [];
 for i = 0 : numeroMax - 1
     disp(strcat('Revisando archivo: Estacion', num2str(i)));
-    [num, intervalos] = GeneraIntervalos(150, 50, i, intervalos);
+    [num, intervalos] = GeneraIntervalos(400, 50, i, intervalos);
     total = num + total;
 end
 
