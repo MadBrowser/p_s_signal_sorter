7f07f6050e48b9063086d5224ba6017ba0804d33
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2014-06-03 18:57:14 -0400

    Se prealloca el tamaño de ExtrTotal para evitar que crezca en cada ciclo, además se cambia la instrucción de concatenar ExtrTotal con los valores por una inserción en la posición correspondiente para mayor velocidad.

--- src/obtaining_extractors/ExtraFinal.m
+++ src/obtaining_extractors/ExtraFinal.m
@@ -3,22 +3,11 @@ function [ExtrTotal] = ExtraFinal(max, datosfinales)
 diary('extractores.txt');
 diary on
 
-cont = 0;
-
-ExtrTotal = [];
+ExtrTotal = zeros(max, 15);
 for i = 1 : max
     disp(strcat('Revisando fila: ',num2str(i)));
-%       if( (~isempty(datosfinales{1,4}) || ~isempty(datosfinales{1,5})) || (cont == 0))
-        [valores] = ObtenerInfo(datosfinales{i,:})
-%          if( (valores(1,14) == 1 || valores(1,15) == 1) || (cont == 0) )
-            ExtrTotal = [ExtrTotal ; valores];
-%          else
-%              cont = cont + 1;
-%          end
-%          if(cont == 95)
-%              cont = 0;
-%          end
-      
+    [valores] = ObtenerInfo(datosfinales{i,:});
+    ExtrTotal(i,:) = valores;
 end
 
 diary off
