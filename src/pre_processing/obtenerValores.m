3e5d5384981dc76f426fa2ad02158d77bd497876
Carlos Vilches Mandiola <carlos.vilches.mandiola@gmail.com>
2014-06-03 17:35:32 -0400

    Corregida la llamada a GeneraIntervalos

--- src/pre_processing/obtenerValores.m
+++ src/pre_processing/obtenerValores.m
@@ -6,11 +6,18 @@ diary on
 % Total estaciones de entrenamiento + validación = 1590
 % Total estaciones de prueba = 1580
 
+% aumento = 100000;
+aumento = 0;
+
+stations_path = '~/Dev/p_s_signal_sorter/stations/Estacion';
+test_stations_path = '~/Dev/p_s_signal_sorter/test_data/stations/Estacion';
+
+
 total = 0;
 intervalos = [];
 for i = 0 : numeroMax - 1
-    disp(strcat('Revisando archivo: Estacion', num2str(i)));
-    [num, intervalos] = GeneraIntervalos(400, 50, i, intervalos);
+    disp(strcat('Revisando archivo: Estacion', num2str(i + aumento)));
+    [num, intervalos] = GeneraIntervalos(400, 50, i + aumento, intervalos, stations_path);
     total = num + total;
 end
 
